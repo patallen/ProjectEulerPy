e369bb4590638aaaaddca5cf360a9b33577da120
Patrick Allen <prallen90@gmail.com>
2016-02-11 08:12:48 -0500

    Use one-liner to feel good about yourself

--- pe20.py
+++ pe20.py
@@ -1,11 +1,5 @@
-# n! means n × (n − 1) × ... × 3 × 2 × 1
-# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# n! means n x (n-1) x ... x 3 x 2 x 1
+# For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
 # and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27
 # Find the sum of the digits in the number 100!
-from operator import mul
-
-
-def sum_factorial(n):
-    return sum([int(x) for x in str(reduce(mul, range(1, n+1)))])
-
-print sum_factorial(100)
+print sum([int(x) for x in str(reduce(lambda a, b: a*b, range(1, 100+1)))])
