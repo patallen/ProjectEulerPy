05420f460075f9b1f6f425086c7215a732200777
Patrick Allen <prallen90@gmail.com>
2016-02-12 08:39:35 -0500

    Complete pe22.py

--- pe22.py
+++ pe22.py
@@ -1,4 +1,11 @@
+import csv
+
 
 def name_score(name="Patrick"):
     scores = {l: s for s, l in enumerate(" abcdefghijklmnopqrstuvwxyz")}
     return sum([scores.get(x) for x in name.lower()])
+
+with open("names.txt") as f:
+    names = list(csv.reader(f, delimiter=","))[0]
+
+print sum(map(lambda (y, x): name_score(x)*y, enumerate(sorted(names), 1)))
